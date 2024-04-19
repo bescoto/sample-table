aa25fa5983cf8e28eb758e22ced7bdefed847589
ben <ben@emerose.org>
2024-04-19 15:31:03 -0500

    Add in a tree view, but tree view isn't connected right now

--- sample-table.js
+++ sample-table.js
@@ -8,49 +8,71 @@ document.addEventListener("DOMContentLoaded", function() {
                 skipEmptyLines: true
             });
 
-            // Initialize DataTable with parsed data
-            var table = $('#data-table').DataTable({
-                data: data.data,
-                columns: data.meta.fields.map(field => ({
-		    title: field,
-		    data: field
-		})),
-		initComplete: function () {
-                    // Setup - add a text input to each footer cell
-                    this.api().columns().every(function () {
-                        var column = this;
-                        var input = $('<input type="text" placeholder="Search '+column.header().innerText+'" />')
-                            .appendTo($(column.header()).empty())
-                            .on('keyup change clear', function () {
-                                if (column.search() !== this.value) {
-                                    column.search(this.value).draw();
-                                }
-                            });
-                    });
-		}
-	    });
+            var table = initTable(data);
+            addTableSearch(table);
 
-            // Setup - add a text input to each header cell
-            $('#data-table thead tr:eq(1) th').each( function (i) {
-                var title = $(this).text();
-                $(this).html('<input type="text" placeholder="Search '+title+'" />');
-
-                $('input', this).on('keyup change', function () {
-                    if (table.column(i).search() !== this.value) {
-                        table
-                            .column(i)
-                            .search(this.value)
-                            .draw();
-                    }
-                });
-            });
-
+	    initJSTree();
+	    
 	    // Add click event listener for rows
             $('#data-table tbody').on('click', 'tr', openRow(data, table));
         })
         .catch(error => console.error('Error loading the CSV file:', error));
 });
 
+// Initialize DataTable with parsed data and return it
+initTable = function(data) {
+    return $('#data-table').DataTable({
+        data: data.data,
+        columns: data.meta.fields.map(field => ({
+	    title: field,
+	    data: field
+	})),
+	initComplete: function () {
+            // Setup - add a text input to each footer cell
+            this.api().columns().every(function () {
+                var column = this;
+                var input = $('<input type="text" placeholder="Search '+column.header().innerText+'" />')
+                    .appendTo($(column.header()).empty())
+                    .on('keyup change clear', function () {
+                        if (column.search() !== this.value) {
+                            column.search(this.value).draw();
+                        }
+                    });
+            });
+	}
+    });
+}
+
+// Add column-level search to the table
+addTableSearch = function(table) {
+    $('#data-table thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search '+title+'" />');
+	
+        $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+                table
+                    .column(i)
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+}
+
+// Initialize the JS tree view
+initJSTree = function() {
+    $('#myjstree').jstree({
+        'core' : {
+            'data' : [
+                { "text" : "Root node", "children" : [
+                    { "text" : "Child node 1" },
+                    { "text" : "Child node 2" }
+                ]}
+            ]
+        }
+    });
+}
 
 // Return callback that can run when the user clicks a table
 openRow = function(data, table) {
@@ -76,6 +98,7 @@ openRow = function(data, table) {
             modal: true,
             width: 400,
             buttons: {
+		"Select from Lake": showTree,
                 "Save Changes": function() {
                     var updatedHeight = $('#edit-height').val();
                     rowData['Height'] = updatedHeight;  // Update the Height in rowData
@@ -88,5 +111,22 @@ openRow = function(data, table) {
 		}
             }
 	});
-    };
+    }
+}
+
+// Open the tree view
+showTree = function() {
+    $("#jstree-container").dialog({
+        modal: true,
+        title: "Tree View",
+        width: 400,
+        open: function () {
+            $("#myjstree").jstree(true).refresh(); // Refresh jsTree
+        },
+	buttons: {
+            Close: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
 }
